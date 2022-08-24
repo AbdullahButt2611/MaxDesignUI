b69494a3793eea66d74b603a26067bc1ed8fe8c3
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-08-24 20:17:00 +0500

    Sidebar Completed

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,31 @@
-const navMenu = document.getElementById('nav-menu');
-const toggleMenu = document.getElementById('toggle-menu');
-const closeMenu = document.getElementById('close-menu');
+// const navMenu = document.getElementById('nav-menu');
+// const toggleMenu = document.getElementById('toggle-menu');
+// const closeMenu = document.getElementById('close-menu');
 
 
-toggleMenu.addEventListener("click", ()=>{
-    navMenu.classList.toggle('show')
-})
+// toggleMenu.addEventListener("click", ()=>{
+//     navMenu.classList.toggle('show')
+// })
 
-closeMenu.addEventListener("click", ()=>{
-    navMenu.classList.remove('show')
-})
+// closeMenu.addEventListener("click", ()=>{
+//     navMenu.classList.remove('show')
+// })
+
+
+
+
+// ==============================       SideBar     ==========================================
+
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".bx-search");
+btn.style.cursor = "pointer";
+searchBtn.style.cursor = "pointer";
+
+btn.onclick = function(){
+    sidebar.classList.toggle("active");
+}
+
+searchBtn.onclick = function(){
+    sidebar.classList.toggle("active");
+}
